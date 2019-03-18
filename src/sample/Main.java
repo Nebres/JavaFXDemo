2ed72963d98745ef4ddfc67debb917f6e9471ea9
Nebres <serbenski@o2.pl>
2019-03-18 23:12:26 +0100

    JavaFX Basics  - Scane and Buttons

---
+++ src/sample/Main.java
@@ -0,0 +1,39 @@
+package sample;
+
+import javafx.application.Application;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+public class Main extends Application {
+
+    private Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("JavaFx Demo");
+
+        button = new Button();
+        button.setText("Click Me!");
+
+        button.setOnAction(event ->  System.out.println("You Clicked!"));
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 800, 600);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+}
